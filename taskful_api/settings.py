87a64a36ad622bf7728c5b8e04899b74352def85
Sanna <iveljas@gmail.com>
2022-03-27 05:54:09 +0200

    aadddfff

--- taskful_api/settings.py
+++ taskful_api/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = '-_vyw6na+6wqn07oqwgcyw87l_g@&jy2#nh1d#+r98a9c7t1!f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['api-house-si.herokuapp.com','127.0.0.1:8000']
+ALLOWED_HOSTS = ["https://api-house-si.herokuapp.com","http://127.0.0.1:8000"]
 #127.0.0.1  # he had
 
 
